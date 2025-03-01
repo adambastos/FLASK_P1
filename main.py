cb04415a45e3fc254dedcfdec5963d44e9b72a5b
adambastos <adambastoss@gmail.com>
2025-03-01 20:18:02 -0300

    .

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/usuarios')
+def usuarios():
+    lista_usuarios = ['João', 'Maria', 'Pedro', 'Ana', 'Carlos']
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
