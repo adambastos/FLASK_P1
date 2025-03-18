77987e42f155753f3cc3a198822f8d80a20646d4
Adam Bastos Santos <adam.santos@anvisa.gov.br>
2025-03-18 16:25:51 -0300

    ...

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1072e926d71a094f491476e564a2443f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:201611@localhost/FLASK_P1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
--- models.py
+++ models.py
@@ -3,4 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.column(db.String(100), nullable)
+    username = db.column(db.String(100), nullable=False) #nullable False diz que não pode ser null   
+    email = db.column(db.String(100), unique=True, nullable=False) 
+    senha = db.column(db.String(100), nullable=False)
